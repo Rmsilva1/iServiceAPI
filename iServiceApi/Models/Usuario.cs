2bc98b2e42d13a9f751c75fcaa616d3317261199
opet <opet@Academico.edu>
2018-06-15 21:43:57 -0300

    Code added

---
+++ iServiceApi/Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iServiceApi.Models
+{
+    public class Usuario
+    {
+        [Key]
+        private long Id { get; set; }
+
+        private int PermissionLevel { get; set; }
+
+        private int IsTecnico { get; set; }
+
+        private String Cpf { get; set; }
+
+        private String Email { get; set; }
+
+        private String Senha { get; set; }
+
+        private String Nome { get; set; }
+
+        private String Telefone { get; set; }
+
+        private String Estado { get; set; }
+
+        private String Cidade { get; set; }
+
+        private String Cep { get; set; }
+
+        private String Bairro { get; set; }
+
+        private String Rua { get; set; }
+
+        private String Complemento { get; set; }
+
+    }
+}
---
+++ iServiceApi/Models/iServiceContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iServiceApi.Models
+{
+    public class IServiceContext : DbContext
+    {
+        public IServiceContext(DbContextOptions<IServiceContext> options): base(options)
+        {
+        }                
+        public DbSet<Usuario> TodoItems { get; set; }
+    }
+}
+
